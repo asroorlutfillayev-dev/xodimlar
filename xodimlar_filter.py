03b28ccbf6ade99814863a2b34b2c53c585fe224
vitoAI <tonycaps1001@gmail.com>
2026-07-04 13:24:58 +0500

    Data Science: Xodimlar loyihasi tayyor

---
+++ xodimlar_filter.py
@@ -0,0 +1,7 @@
+xodimlar = {
+    "Aziz": 2500000,
+    "Malika": 4200000,
+    "Sardor": 3100000,
+    "Nodira": 1800000
+}
+boy_xodimlar = [ism for ism, maosh in xodimlar.items() if maosh > 3000000]
